49817c251d414ed24280197731b2f591febafb10
Lucas Estefano <lucccasestefano_@hotmail.com>
2020-09-19 11:40:31 -0300

    Ajustando a estrutura e criando o primeiro component

---
+++ to-do-list/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+import Tarefas from "./componentes/Tarefas";
+
+function App() {
+  return (
+    <div className="App">
+      <Tarefas/>
+    </div>
+  );
+}
+
+export default App;
---
+++ to-do-list/src/componentes/Tarefas.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+export default class Tarefas extends Component{
+
+    render(){
+        return <h1>Tarefas </h1>;
+    }
+
+}
