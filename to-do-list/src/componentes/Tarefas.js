f2215da49e81b624d188b082ea831c75839ed3f9
Lucas Estefano <lucccasestefano_@hotmail.com>
2020-09-19 12:28:20 -0300

    Concluindo o To-Do list

--- to-do-list/src/componentes/Tarefas.js
+++ to-do-list/src/componentes/Tarefas.js
@@ -2,18 +2,44 @@ import React, { Component, Fragment } from "react";
 
 export default class Tarefas extends Component {
 
+    constructor(){
+        super();
+        this.state = {
+            tarefa : "",
+            tarefas : ["Adicione as tarefas"]
+        };
+
+        this.adicionaTarefa = this.adicionaTarefa.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    }
+
     render() {
-        const tarefas = ["Tarefa 1, Tarefa 2, Tarefa 3,"];
         return (
             <Fragment>
                 <h1>To-Do List </h1>
+                <input onChange = {this.handleChange} value ={ this.state.tarefa }/>
+                <button onClick={this.adicionaTarefa}>Adicionar</button>
                 <ul>
-                    <li>Tarefa 1 </li>
-                    <li>Tarefa 2 </li>
-                    <li>Tarefa 3 </li>
+                    { this.state.tarefas.map(tarefa => <li>{ tarefa }</li>)}
                 </ul>
             </Fragment>
         );
+        
+       
+
+
+    }
+
+    adicionaTarefa(){
+          this.setState({
+              tarefa : "",
+               tarefas : [].concat(this.state.tarefas, this.state.tarefa)
+            }); 
+    }
+
+
+    handleChange(event){
+        this.setState({tarefa : event.target.value})
     }
 
 }
